b763a726ab6ed58b57e98d6913f66b068d979b04
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-12-16 00:14:07 -0300

    chore: inital project

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+export default {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
+
+// Parei em 37:52 da Aula 02 - 15-12-2022 17:05:12
